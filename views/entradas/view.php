dc2ff5491af25430ef257e7a9fe3290532afa849
Jesús Federico Franco <j.francomedinilla@gmail.com>
2017-02-01 16:20:30 +0100

    Entradas retocadas

--- views/entradas/view.php
+++ views/entradas/view.php
@@ -1,7 +1,8 @@
 <?php
 
+use yii\helpers\Url;
 use yii\helpers\Html;
-use yii\widgets\DetailView;
+use app\models\Entrada;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Entrada */
@@ -12,50 +13,31 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="entrada-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <article class="entrada" data-key="<?= $model->id; ?>"style="font-size: 20px; font-family: Verdana;" >
+        <h2 class="title" >
+        <?= Html::a(Html::encode($model->titulo), $model->url, ['titulo' => $model->titulo]) ?>
+        </h2>
+        <p>por <?= $model->usuario->username ?> el <?= Yii::$app->formatter->asDate($model->created_at) ?>
+        publicado <?= Yii::$app->formatter->asRelativeTime($model->created_at) ?> </p>
+        <p><?= Html::encode($model->texto) ?></p>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+        <?= Html::a(Html::encode('comentarios'), Url::to(['entradas/view', 'id' => $model->id])) ?>
+    </article>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'url:url',
-            'titulo',
-            'texto',
-            'created_at:datetime',
-            [
-                'label' => 'Categoría',
-                'attribute' => 'categoria.nombre',
-            ]
-        ],
-    ]) ?>
+    <?php
+    if ($model->usuario->id == Yii::$app->user->id) { ?>
+        <p>
+            <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p> <?php
+        } ?>
 
-    <?php echo \yii2mod\comments\widgets\Comment::widget([
-      'model' => $model,
-      'maxLevel' => 2,
-      // set `pageSize` with custom sorting
-      'dataProviderConfig' => [
-          'sort' => [
-              'attributes' => ['id'],
-              'defaultOrder' => ['id' => SORT_DESC],
-          ],
-          'pagination' => [
-              'pageSize' => 10
-          ],
-      ],
-       // your own config for comments ListView, for example:
-      'listViewConfig' => [
-          'emptyText' => Yii::t('app', 'No comments found.'),
-      ]
-]); ?>
+
 
 </div>
